c35411d9da60d8ac4ac6193f3625958fbe93d5b2
Sebastian Wang <43295730+Lemon2ee@users.noreply.github.com>
2024-04-06 18:30:49 -0400

    server init w/ args

--- src/server.py
+++ src/server.py
@@ -1,4 +1,33 @@
+import argparse
 
-SERVER_ADDRESS = 
 
-def login:
+class Server:
+    """
+    A server class for managing connections and communications in a basic chat application.
+    """
+
+    def __init__(self, port):
+        """
+        Initialize the server with a specific port and default IP.
+        """
+        self.port = port
+        self.ip = "127.0.0.1"
+        self.user_info = {}
+
+    def listen(self):
+        pass
+
+
+def parse_arguments():
+    """
+    Parse command line arguments for the server.
+    """
+    parser = argparse.ArgumentParser(description="Instant message exchange app, server side")
+    parser.add_argument('-sp', type=int, help='Server port to bind')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    server = Server(args.sp)
+    server.listen()
